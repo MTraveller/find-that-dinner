80f45dfa95bde8c439327a2ea4927c53c342a1c7
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-22 19:33:15 +0000

    api updated

--- assets/js/apis.js
+++ assets/js/apis.js
@@ -1,4 +1,4 @@
-let firstEndpoint = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=';
-let lastEndpoint = '&app_id=db107cf3&app_key=fe82b203fb1dcc311c67cd62001244ca&mealType=Dinner&imageSize=REGULAR';
+let firstEndpoint = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=';
+let lastEndpoint = '&app_id=db107cf3&app_key=fe82b203fb1dcc311c67cd62001244ca&mealType=Dinner&imageSize=REGULAR&random=true';
 
 export { firstEndpoint, lastEndpoint };
