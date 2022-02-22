ce5641d9857206f9691a10c3abe624899fd0d11f
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-22 15:52:08 +0000

    refactor code to handle submit and fetch results

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -1,3 +1,13 @@
-const form = document.querySelector('[name="search"]');
+import { form } from './forms.js';
+import { displayRecipes } from './utils.js';
 
-export { form };
+async function handleSubmit(e) {
+    e.preventDefault();
+    const el = e.currentTarget;
+    const recipes = await displayRecipes(el.keyword.value)
+    console.log(recipes);
+}
+
+form.addEventListener('submit', handleSubmit);
+
+export { handleSubmit };
