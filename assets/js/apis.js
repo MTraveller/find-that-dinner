e074344c75ba7cfae0736c6ac945d2f1cedb2ef2
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-22 15:22:15 +0000

    breaking up the api

--- assets/js/apis.js
+++ assets/js/apis.js
@@ -1,5 +1,4 @@
-const endpoint = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=chicken&app_id=db107cf3&app_key=fe82b203fb1dcc311c67cd62001244ca&mealType=Dinner&imageSize=REGULAR';
+let firstEndpoint = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=';
+let lastEndpoint = '&app_id=db107cf3&app_key=fe82b203fb1dcc311c67cd62001244ca&mealType=Dinner&imageSize=REGULAR';
 
-const apiPromise = fetch(endpoint);
-
-export { apiPromise };
+export { firstEndpoint, lastEndpoint };
