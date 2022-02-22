59a912e79c1adfa8b710640b868970a6e39beba0
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-22 19:35:44 +0000

    updated function argument

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,6 @@
 import { firstEndpoint, lastEndpoint } from './apis.js';
 
-async function fetchRecipes(query = 'chicken') {
+async function fetchRecipes(query) {
     const response = await fetch(`${firstEndpoint}${query}${lastEndpoint}`);
     const data = await response.json();
     const results = data.hits;
