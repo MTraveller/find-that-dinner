b5658e2b334f94b740f34a877b25bebe15722c1b
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-22 15:57:39 +0000

    naming update

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -1,10 +1,10 @@
 import { form } from './forms.js';
-import { displayRecipes } from './utils.js';
+import { fetchRecipes } from './utils.js';
 
 async function handleSubmit(e) {
     e.preventDefault();
     const el = e.currentTarget;
-    const recipes = await displayRecipes(el.keyword.value)
+    const recipes = await fetchRecipes(el.keyword.value)
     console.log(recipes);
 }
 
