c6f30340d146b21d6be314255b8338a60ca07552
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-23 15:27:39 +0000

    code refactored from handlers.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,7 @@
 import { firstEndpoint, lastEndpoint } from './apis.js';
+import { displayRecipes } from './scripts.js';
 
+// Fetches the search from api.edemame.com
 async function fetchRecipes(query) {
     const response = await fetch(`${firstEndpoint}${query}${lastEndpoint}`);
     const data = await response.json();
@@ -7,4 +9,20 @@ async function fetchRecipes(query) {
     return results;
 }
 
-export { fetchRecipes };
+// Sends the search request to fetchRecipes()
+async function fetchSubmit(e) {
+    e.preventDefault();
+    const formButton = e.currentTarget;
+  
+    // Temporary disable the search button
+    formButton.search.disabled = true;
+  
+    // Fetch and display the results of the searched keyword
+    const recipesResults = await fetchRecipes(formButton.keyword.value);
+    displayRecipes(recipesResults);
+  
+    // Enable the search button again
+    formButton.search.disabled = false;
+  }
+
+export { fetchSubmit };
