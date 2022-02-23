26b7f9a36b3222ce51c6a17157f81e14f136c3c2
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-23 21:34:01 +0000

    refactored code added global results array

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,24 @@
 import { firstEndpoint, lastEndpoint } from './apis.js';
 import { displayRecipes } from './scripts.js';
 
+let results = [];
+
+function fetchRecipe(recipeLabel) {
+  console.log(results);
+  console.log(recipeLabel);
+}
+
+// Generate the html for the recipeCards
+function recipeCardsGen(recipes) {
+  const genHTML = recipes.map((item) =>    
+    `<figure class="recipe-card-image"><img src="${item.recipe.images.REGULAR.url}" title="${item.recipe.label}" alt="${item.recipe.label}"></figure>
+      <h2 class="recipe-card-title">${item.recipe.label}</h2>
+      <span class="recipe-card-info">${Math.floor(item.recipe.calories)} calories</span><span class="recipe-card-info-divider">|</span><span class="recipe-card-info">${item.recipe.ingredients.length} ingredients</span>
+      <h3 class="recipe-card-source">${item.recipe.source}</h3>`
+  );
+  displayRecipes(genHTML);
+}
+
 // Fetches the search from api.edemame.com
 async function fetchRecipes(query) {
     const response = await fetch(`${firstEndpoint}${query}${lastEndpoint}`);
@@ -19,10 +37,12 @@ async function fetchSubmit(e) {
   
     // Fetch and display the results of the searched keyword
     const recipesResults = await fetchRecipes(formButton.keyword.value);
-    displayRecipes(recipesResults);
+    recipeCardsGen(recipesResults);
   
     // Enable the search button again
     formButton.search.disabled = false;
-  }
 
-export { fetchSubmit };
+    return results = [...recipesResults];
+}
+
+export { fetchSubmit, fetchRecipe };
