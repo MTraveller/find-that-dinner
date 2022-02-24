364d7e900cca9c24023939f6db7042e46c78e384
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-24 09:31:29 +0000

    added comments to code

--- assets/js/apis.js
+++ assets/js/apis.js
@@ -1,8 +1,10 @@
 import { recipeCardsGen } from './utils.js'
 
+// Break up the URL from / to the query
 let firstEndpoint = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=';
 let lastEndpoint = '&app_id=db107cf3&app_key=fe82b203fb1dcc311c67cd62001244ca&mealType=Dinner&imageSize=REGULAR&random=true';
 
+// Save fetch result in a global variable
 let fetchResults = [];
 
 // Fetches the search from api.edemame.com
