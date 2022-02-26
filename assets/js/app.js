804be2a37fbcc9decbf45bcfdd7aae8dcd7f1737
MTraveller <26313521+MTraveller@users.noreply.github.com>
2022-02-26 13:22:54 +0000

    code refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,14 @@ import { drawChart } from './utils.js';
 function displaySingleRecipe(producedSingleHTML) {
   console.log(producedSingleHTML);
   // Grabs the aside after it has been printed on document
-  let recipeCard = body.querySelector('aside');
+  let recipeCard = body.querySelector('section');
 
   // Check 'aside' exists and remove it if it does to be replaced by the new clicked recipe
   if (recipeCard) body.removeChild( body.firstChild );
 
   // Create aside modal and print it on the document
-  let cardAside = document.createElement('aside');
-  cardAside.classList.add('recipe-card','single');
+  let cardAside = document.createElement('section');
+  cardAside.classList.add('recipe-card-single');
   cardAside.innerHTML = producedSingleHTML;
   body.prepend(cardAside);
 
@@ -28,7 +28,7 @@ function displayRecipes(producedHTML) {
   let cardDivs = recipesGrid.querySelectorAll('.recipe-card');
 
   // Check cards exists and replace them with new fetched recipes
-  if (cardDivs.length > 1) while ( recipesGrid.firstChild ) recipesGrid.removeChild( recipesGrid.firstChild );
+  if (cardDivs.length > 1) while (recipesGrid.firstChild) recipesGrid.removeChild(recipesGrid.firstChild);
 
   // Iterates through the HTML array produced for fetched recipes and prints them on the document
   producedHTML.forEach(htmlResult => {
