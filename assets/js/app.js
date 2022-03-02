872071a270859ce8451022e39a5b16088dc9368b
MTraveller <m.tantouri@gmail.com>
2022-03-02 17:33:18 +0000

    code for displayHelp function

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,39 @@
 import { recipesGrid, grabRecipeCard, body, cardSection, handleCloseModal } from './handlers.js';
 import { genGooglePie } from './utils.js';
 
-function displayHelp() {
-  return console.log('Hello!')
+// Display the generated help HTML
+function displayHelp(producedHelpHTML) {
+  // Grab the modal section
+  const help = cardSection.querySelector('article');
+  // Check if 'article' element exists and remove it if it does
+  if (help) cardSection.removeChild(cardSection.firstChild);
+
+  // Create article modal
+  const cardArticle = document.createElement('article');
+  cardArticle.innerHTML = producedHelpHTML;
+  
+  // Start at the top of the modal
+  cardSection.scrollTo(0, 0);
+
+  // Temporary add class to content
+  body.style.overflow = 'hidden';
+
+  // Temporary add class to section
+  cardSection.classList.add('active');
+
+  // Print help content to the document
+  cardSection.prepend(cardArticle);
+
+  // Fire the close modal eventListener in handlers.js
+  return handleCloseModal();
 }
 
+// Display the generated recipe HTML
 function displaySingleRecipe(producedSingleHTML) {
   // Grab the recipe section modal
   const recipeCard = body.querySelector('article');
 
-  // Check if 'article' element exists and remove it if it does, to be replaced by the new clicked recipe
+  // Check if 'article' element exists and remove it if it does
   if (recipeCard) cardSection.removeChild(cardSection.firstChild);
 
   // Create article modal
